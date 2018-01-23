162ac073f8080805370f819e326baea4af04f64b
Gavin Loughridge <gavinlough@gmail.com>
2018-01-23 11:49:48 -0600

    refactored to not use session

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,11 +6,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    if params[:task_list_id]
-      session["task_list_id"] = params[:task_list_id]
-    end
-
-    task_params = params.require(:task).permit(:description, :due_date).merge(task_list_id: session["task_list_id"])
+    task_params = params.require(:task).permit(:description, :due_date).merge(task_list_id: params[:task_list_id])
     @task = Task.new(task_params)
     if @task.save
       redirect_to root_path, notice: "Task was created successfully!"
